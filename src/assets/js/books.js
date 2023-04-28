98e503938a05d3e8505bf54a889fe7f9d1ef6aac
Gimba Manasseh A <manasseh.gimba.a@gmail.com>
2023-04-28 16:16:03 +0100

    Image optimization src folder
    added assets then in it added css, js and media folders and did a proper image placement for easy optimization upon build

---
+++ src/assets/js/books.js
@@ -0,0 +1,23 @@
+import img1 from "../media/images/letter-to-trump.jpg";
+import img2 from "../media/images/atomic-habits.jpg";
+import img3 from "../media/images/psychology-of-money.jpg";
+export const books = [
+  {
+    author: "Donald J Trump",
+    title: "Letters to Trump",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Morgan Housel",
+    title: "The Psychology of Money",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,53 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/assets/css/index.css";
-
-const firstBook = {
-  author: "Donald J Trump",
-  title: "Letters to Trump",
-  img: "./images/letter-to-trump.jpg",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "./images/atomic-habits.jpg",
-};
-
+import { books } from "../src/assets/js/books";
+import Book from "../src/assets/js/Book";
 function Booklist() {
   return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
+    <>
+      <h2 className="app-title">Amazon Best Sellers Mockup ReactApp</h2>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 }
-
-const Book = (props) => {
-  console.log(props);
-  return (
-    <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
-    </article>
-  );
-};
-
-// const Image = () => (
-//   <img src="./images/letter-to-trump.jpg" alt="Letters to Trump" />
-// );
-// const Title = () => <h2>Letters to Trump</h2>;
-// const Author = () => {
-//   return <h4>Donald J Trump</h4>;
-// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Booklist />);
