cfe95ffed2d7454dea1d621fec3b84d091c87f28
Gimba Manasseh A <manasseh.gimba.a@gmail.com>
2023-07-20 12:12:55 +0100

    test

---
+++ src/Tests.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export function Tests({}) {
+  return <div>
+        <h3>Hello World</h3>
+      </div>;
+}
+  
---
+++ src/test.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+export function Test({}) {
+  return <div>
+        <h3>Hello World</h3>
+      </div>;
+}import React from "react";
+export function Test({}) {
+  return <div>
+        <h3>Hello World</h3>
+      </div>;
+}
+
+export function Test({}) {
+  return <div>
+        <h3>Hello World</h3>
+      </div>;
+}
+
+export function Test({}) {
+  return <div>
+        <h3>Hello World</h3>
+      </div>;
+}
+      
